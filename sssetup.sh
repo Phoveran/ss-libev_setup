4ca7ae866cd7c387871a135a2844bd1416dbfd46
Phoveran <dauntlessstan@gmail.com>
2019-03-12 19:36:27 +0800

    Update sssetup.sh

--- sssetup.sh
+++ sssetup.sh
@@ -9,37 +9,37 @@ read ssport
 echo -e "请输入密码：\c"
 read sskey
 #安装依赖文件
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt-get install --no-install-recommends gettext build-essential autoconf libtool libpcre3-dev asciidoc xmlto libev-dev libc-ares-dev automake libmbedtls-dev libsodium-dev -y
-sudo apt install git -y
-sudo apt install screen -y
+apt update -y
+apt upgrade -y
+apt-get install --no-install-recommends gettext build-essential autoconf libtool libpcre3-dev asciidoc xmlto libev-dev libc-ares-dev automake libmbedtls-dev libsodium-dev -y
+apt install git -y
+apt install screen -y
 #安装Libsodium
 export LIBSODIUM_VER=1.0.17
 wget https://download.libsodium.org/libsodium/releases/libsodium-$LIBSODIUM_VER.tar.gz
 tar xvf libsodium-$LIBSODIUM_VER.tar.gz
 pushd libsodium-$LIBSODIUM_VER
 ./configure --prefix=/usr && make
-sudo make install
+make install
 popd
-sudo ldconfig
-sudo rm -r libsodium-$LIBSODIUM_VER
+ldconfig
+rm -r libsodium-$LIBSODIUM_VER
 #安装MbedTLS
 export MBEDTLS_VER=2.16.0
 wget https://tls.mbed.org/download/mbedtls-$MBEDTLS_VER-gpl.tgz
 tar xvf mbedtls-$MBEDTLS_VER-gpl.tgz
 pushd mbedtls-$MBEDTLS_VER
 make SHARED=1 CFLAGS="-O2 -fPIC"
-sudo make DESTDIR=/usr install
+make DESTDIR=/usr install
 popd
-sudo ldconfig
-sudo rm -r mbedtls-$MBEDTLS_VER
+ldconfig
+rm -r mbedtls-$MBEDTLS_VER
 #安装shadowsocks-libev
 git clone https://github.com/shadowsocks/shadowsocks-libev.git
 cd shadowsocks-libev
 git submodule update --init --recursive
 ./autogen.sh && ./configure && make
-sudo make install
+make install
 cd ~
 #开启bbr
 modprobe tcp_bbr
